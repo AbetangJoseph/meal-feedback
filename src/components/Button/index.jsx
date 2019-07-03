03fc95eaa760d6797525a8e65f936046664d1333
Abetang Joseph <josephabetang@gmail.com>
2019-07-03 16:38:23 +0100

    change class to className

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Button({ buttonType, buttonWidth, buttonLabel }) {
   return (
-    <button type="button" class={buttonType} style={{ width: buttonWidth }}>
+    <button type="button" className={buttonType} style={{ width: buttonWidth }}>
       {buttonLabel}
     </button>
   );
