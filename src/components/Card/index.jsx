e1e3c20641c0b37b8cee17df5145d1ca85603454
Abetang Joseph <josephabetang@gmail.com>
2019-07-03 17:51:59 +0100

    add css file, remove inline style and removed messageFontsize prop

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
+import './style.css';
 
-export default function Card({
-  cardTitle,
-  cardBody,
-  cardWidth,
-  message,
-  messageFontsize
-}) {
+export default function Card({ cardTitle, cardBody, cardWidth, message }) {
   return (
     <>
       <div className={cardWidth}>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title text-muted">{cardTitle}</h5>
-            <p style={{ fontSize: messageFontsize }}>{message}</p>
+            <p className="login-p-tags">{message}</p>
             <hr />
             <div className="card-text" style={{ listStyleType: 'none' }}>
               {cardBody}
             </div>
+            <hr />
+            <p className="login-p-tags">
+              Have an account alerady? <a href="#">Login</a>
+            </p>
           </div>
         </div>
       </div>
