773ac01443a84f0f212740c13535b0ea78a7c443
Abetang Joseph <josephabetang@gmail.com>
2019-07-04 05:15:11 +0100

    create dashboard component

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+import Card from '../../components/Card';
+import Button from '../../components/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+export default function Dasboard() {
+  return (
+    <div style={{ margin: '25px' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
+        <h5>
+          <Link to="/dashboard" className="nav-link dashboard-headers">
+            dashboard
+          </Link>
+        </h5>
+        <h5>
+          <Link to="/history" className="nav-link dashboard-headers">
+            my ratings
+          </Link>
+        </h5>
+      </div>
+      <hr />
+      <div
+        className="row"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginBottom: '50px'
+        }}
+      >
+        <Card
+          cardWidth="col-sm-3"
+          cardTitle="Breakfast"
+          cardBody="Not available"
+          footerMessage={
+            <Button
+              buttonType="btn btn-outline-danger"
+              icon={<FontAwesomeIcon icon={faPaperPlane} />}
+              buttonLabel="feedback"
+            />
+          }
+        />
+        <Card
+          cardWidth="col-sm-3"
+          cardTitle="Lunch"
+          cardBody="Not available"
+          footerMessage={
+            <Button
+              buttonType="btn btn-outline-danger"
+              icon={<FontAwesomeIcon icon={faPaperPlane} />}
+              buttonLabel="feedback"
+            />
+          }
+        />
+        <Card
+          cardWidth="col-sm-3"
+          cardTitle="Dinner"
+          cardBody="Not available"
+          footerMessage={
+            <Button
+              buttonType="btn btn-outline-danger"
+              icon={<FontAwesomeIcon icon={faPaperPlane} />}
+              buttonLabel="feedback"
+            />
+          }
+        />
+      </div>
+    </div>
+  );
+}
