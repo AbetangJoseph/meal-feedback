d7c21ca5e634f83a2da0290a900e14e804b2d534
Abetang Joseph <josephabetang@gmail.com>
2019-07-04 05:02:39 +0100

    add Link to Signup

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '../../components/Card';
 import InputField from '../../components/InputField';
 import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function Login() {
@@ -23,7 +24,9 @@ export default function Login() {
         }
         footerMessage={
           <span>
-            Have an account alerady? <a href="/">Login</a>
+            <Link to="/signup" className="nav-link dashboard-headers">
+              Don't have an account? Signup
+            </Link>
           </span>
         }
       />
