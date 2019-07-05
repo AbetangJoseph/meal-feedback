1af31586d2986a1e320fdcb579baa7a5bfec2df4
Abetang Joseph <josephabetang@gmail.com>
2019-07-05 03:10:46 +0100

    add rating card

--- src/pages/UserRatings/index.jsx
+++ src/pages/UserRatings/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
+import Card from '../../components/Card';
 
 export default function userRatings() {
+  let date = new Date(Date.now());
+
   return (
     <div style={{ margin: '25px' }}>
       <div className="my-ratings">
@@ -13,6 +16,23 @@ export default function userRatings() {
           </Link>
         </h5>
         <h5>my ratings</h5>
+      </div>
+      <div style={{ margin: '15px' }}>
+        <div
+          className="row"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginBottom: '50px'
+          }}
+        >
+          <Card
+            cardTitle={<p style={{ color: '#cf320d' }}>food name</p>}
+            cardBody={<h6>feedback</h6>}
+            cardWidth="col-sm-8"
+            footerMessage={<p>{date.toString().split('G')[0]}</p>}
+          />
+        </div>
       </div>
     </div>
   );
