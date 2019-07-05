52dfb3cac6cfdcf6899c6804f328e5f05354b715
Abetang Joseph <josephabetang@gmail.com>
2019-07-05 01:35:01 +0100

    add myratings component to route

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
+import userRatings from '../pages/UserRatings';
+
 import { Route, Switch } from 'react-router-dom';
 
 const Routes = () => (
@@ -9,6 +11,7 @@ const Routes = () => (
     <Route exact path="/" component={Dashboard} />
     <Route path="/login" component={Login} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/myratings" component={userRatings} />
   </Switch>
 );
 
