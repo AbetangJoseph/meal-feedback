a824b65cb81b1ceaf78254f043dac8c1768b8d86
Abetang Joseph <josephabetang@gmail.com>
2019-07-05 19:39:34 +0100

    add InputField props and Icons

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,6 +3,8 @@ import Card from '../../components/Card';
 import InputField from '../../components/InputField';
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock, faAt } from '@fortawesome/free-solid-svg-icons';
 import './style.css';
 
 export default function Login() {
@@ -14,8 +16,31 @@ export default function Login() {
         message="Please fill in this form to login!"
         cardBody={
           <>
-            <InputField />
-            <InputField />
+            <InputField
+              inputType="email"
+              placeholder="email"
+              inputName="email"
+              inputId="email"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faAt}
+                  style={{ color: 'grey', fontSize: '14px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="password"
+              inputName="password"
+              inputId="password"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: 'grey', fontSize: '15px' }}
+                />
+              }
+            />
+
             <Button
               buttonLabel="Login"
               buttonType="btn btn-custom-outline btn-block"
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,6 +4,13 @@ import InputField from '../../components/InputField';
 import Button from '../../components/Button';
 import './style.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPhoneAlt,
+  faLock,
+  faPenAlt,
+  faAt
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function SignUp() {
   return (
@@ -14,12 +21,78 @@ export default function SignUp() {
         message="Please fill in this form to create an account!"
         cardBody={
           <>
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
+            <InputField
+              inputType="text"
+              placeholder="first name"
+              inputName="firstname"
+              inputId="firstname"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPenAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="last name"
+              inputName="lastname"
+              inputId="lastname"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPenAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="email"
+              placeholder="email"
+              inputName="email"
+              inputId="email"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faAt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="phone"
+              placeholder="phone"
+              inputName="phone"
+              inputId="phone"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPhoneAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="password"
+              inputName="password"
+              inputId="password"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: 'grey', fontSize: '14px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="confirm password"
+              inputName="confirmpassword"
+              inputId="confirmpassword"
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: 'grey', fontSize: '14px' }}
+                />
+              }
+            />
             <Button
               buttonLabel="sign up"
               buttonType="btn btn-custom-outline btn-block"
