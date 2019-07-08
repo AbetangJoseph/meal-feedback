62dc218b7bcbf676dd7caba02fbdb79288c9dc05
Abetang Joseph <josephabetang@gmail.com>
2019-07-08 14:38:29 +0100

    add inputValue and onChange props

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -5,20 +5,27 @@ export default function InputField({
   inputType,
   placeholder,
   inputId,
-  inputName
+  inputName,
+  change,
+  inputValue
 }) {
   return (
     <>
       <div className="input-group mb-2 mr-sm-2">
-        <div className="input-group-prepend">
-          <div className="input-group-text">{inputSign}</div>
-        </div>
+        {inputSign && (
+          <div className="input-group-prepend">
+            <div className="input-group-text">{inputSign}</div>
+          </div>
+        )}
+
         <input
           type={inputType}
           className="form-control"
           id={inputId}
           placeholder={placeholder}
           name={inputName}
+          onChange={change}
+          value={inputValue}
         />
       </div>
     </>
