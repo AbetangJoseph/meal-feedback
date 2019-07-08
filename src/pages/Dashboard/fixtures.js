fa16e80cab8ed7fb8e2b14315b8f78a456db12ce
Abetang Joseph <josephabetang@gmail.com>
2019-07-08 14:43:37 +0100

    create data for food cards

---
+++ src/pages/Dashboard/fixtures.js
@@ -0,0 +1,23 @@
+export const foodCards = [
+  {
+    cardTitle: 'Breakfast',
+    labelName: 'breakfast',
+    option1: 'verygoodbreakfast',
+    option2: 'goodbreakfast',
+    option3: 'badreakfast'
+  },
+  {
+    cardTitle: 'Lunch',
+    labelName: 'lunch',
+    option1: 'verygoodlunch',
+    option2: 'goodlunch',
+    option3: 'badlunch'
+  },
+  {
+    cardTitle: 'Dinner',
+    labelName: 'dinner',
+    option1: 'verygooddinner',
+    option2: 'gooddinner',
+    option3: 'baddinner'
+  }
+];
