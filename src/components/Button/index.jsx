af45e82939f824516b3d079fbb4b012fdbd94a88
Abetang Joseph <josephabetang@gmail.com>
2019-07-08 14:42:12 +0100

    change type from botton to submit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './style.css';
 
-export default function Button({ buttonType, buttonLabel, icon }) {
+export default function Button({
+  buttonType,
+  buttonLabel,
+  icon,
+  click,
+  cardName,
+  isDisabled
+}) {
   return (
-    <button id="button" type="button" className={buttonType}>
+    <button
+      id="button"
+      type="submit"
+      onClick={click}
+      className={buttonType}
+      name={cardName}
+      disabled={isDisabled}
+    >
       {icon} {buttonLabel}
     </button>
   );
