e2e854d006e04b2411a87141fbdc3c112869b457
Abetang Joseph <josephabetang@gmail.com>
2019-07-29 15:27:51 +0100

    add inputValue

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -36,106 +36,112 @@ export default function SignUp() {
 
   return (
     <div id="signup-card">
-      {/* <> */}
-        <Card
-          cardTitle="Sign Up"
-          cardWidth="col-sm-4"
-          message="Please fill in this form to create an account!"
-          cardBody={
-            <form onSubmit={handleSubmit} className='form-group'>
-              <InputField
-                inputType="text"
-                placeholder="first name"
-                inputName="firstname"
-                inputId="firstname"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faPenAlt}
-                    style={{ color: 'grey', fontSize: '13px' }}
-                  />
-                }
-              />
-              <InputField
-                inputType="password"
-                placeholder="last name"
-                inputName="lastname"
-                inputId="lastname"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faPenAlt}
-                    style={{ color: 'grey', fontSize: '13px' }}
-                  />
-                }
-              />
-              <InputField
-                inputType="email"
-                placeholder="email"
-                inputName="email"
-                inputId="email"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faAt}
-                    style={{ color: 'grey', fontSize: '13px' }}
-                  />
-                }
-              />
-              <InputField
-                inputType="phone"
-                placeholder="phone"
-                inputName="phone"
-                inputId="phone"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faPhoneAlt}
-                    style={{ color: 'grey', fontSize: '13px' }}
-                  />
-                }
-              />
-              <InputField
-                inputType="password"
-                placeholder="password"
-                inputName="password"
-                inputId="password"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faLock}
-                    style={{ color: 'grey', fontSize: '14px' }}
-                  />
-                }
-              />
-              <InputField
-                inputType="password"
-                placeholder="confirm password"
-                inputName="confirmpassword"
-                inputId="confirmpassword"
-                change={handleChange}
-                inputSign={
-                  <FontAwesomeIcon
-                    icon={faLock}
-                    style={{ color: 'grey', fontSize: '14px' }}
-                  />
-                }
-              />
-              <Button
-                buttonLabel="sign up"
-                buttonType="btn btn-custom-outline btn-block"
-              />
-            </form>
-          }
-          footerMessage={
-            <span>
-              <Link to="/login" className="nav-link dashboard-headers">
-                Have an account alerady? Login
-              </Link>
-            </span>
-          }
-        />
-      {/* <> */}
+      <>
+      <Card
+        cardTitle="Sign Up"
+        cardWidth="col-sm-4"
+        message="Please fill in this form to create an account!"
+        cardBody={
+          <form onSubmit={handleSubmit} className="form-group">
+            <InputField
+              inputType="text"
+              placeholder="first name"
+              inputName="firstname"
+              inputId="firstname"
+              inputValue={userSignup.firstname}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPenAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="text"
+              placeholder="last name"
+              inputName="lastname"
+              inputId="lastname"
+              inputValue={userSignup.lastname}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPenAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="email"
+              placeholder="email"
+              inputName="email"
+              inputId="email"
+              inputValue={userSignup.email}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faAt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="phone"
+              placeholder="phone"
+              inputName="phone"
+              inputId="phone"
+              inputValue={userSignup.phone}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faPhoneAlt}
+                  style={{ color: 'grey', fontSize: '13px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="password"
+              inputName="password"
+              inputId="password"
+              inputValue={userSignup.password}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: 'grey', fontSize: '14px' }}
+                />
+              }
+            />
+            <InputField
+              inputType="password"
+              placeholder="confirm password"
+              inputName="confirmpassword"
+              inputId="confirmpassword"
+              inputValue={userSignup.confirmpassword}
+              change={handleChange}
+              inputSign={
+                <FontAwesomeIcon
+                  icon={faLock}
+                  style={{ color: 'grey', fontSize: '14px' }}
+                />
+              }
+            />
+            <Button
+              buttonLabel="sign up"
+              buttonType="btn btn-custom-outline btn-block"
+            />
+          </form>
+        }
+        footerMessage={
+          <span>
+            <Link to="/login" className="nav-link dashboard-headers">
+              Have an account alerady? Login
+            </Link>
+          </span>
+        }
+      />
+      </>
     </div>
   );
 }
