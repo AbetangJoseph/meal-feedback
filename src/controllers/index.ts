d21c4aa21561ace3be0d32993df34ff3f9526f98
Abetang Joseph <josephabetang@gmail.com>
2019-08-01 14:05:59 +0100

    add userLogin

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 import { User } from '../models/schema';
+import 'dotenv/config';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
@@ -15,4 +16,28 @@ const createUser = async (userInfo: any) => {
   throw Error('This user already exists');
 };
 
-export { createUser };
+const userLogin = async (userInfo: any) => {
+  const foundUser = await User.findOne({ email: userInfo.email });
+
+  if (!foundUser) {
+    throw Error('Invalid email or password');
+  }
+
+  const validPassword = await bcrypt.compare(
+    userInfo.password,
+    foundUser.password
+  );
+
+  if (!validPassword) {
+    throw Error('Invalid email or password');
+  }
+
+  const token = jwt.sign(
+    { firstname: foundUser.firstName, _id: foundUser.id },
+    `${process.env.rate_meal_jwt}`
+  );
+
+  return { token };
+};
+
+export { createUser, userLogin };
