b0011ed5ea437680421c27389d67fc5b8c056fb4
Abetang Joseph <josephabetang@gmail.com>
2019-08-01 12:55:41 +0100

    add graphql rootquery

---
+++ src/typeDefs/schema.ts
@@ -0,0 +1,50 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLSchema
+} from 'graphql';
+import { userType } from './types';
+import { createUser, userLogin } from '../controllers';
+
+const query = new GraphQLObjectType({
+  name: 'Queries',
+  description: 'All requests to graphql servers',
+  fields: {
+    login: {
+      type: userType,
+      description: 'this gets a single user',
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve: (_source, args) => userLogin(args)
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutations',
+  description: 'All operations to store, update and update data',
+  fields: {
+    createUser: {
+      type: userType,
+      description: 'creates a single user',
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        phone: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (_source, args) => createUser(args)
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query,
+  mutation
+});
+
+export default schema;
