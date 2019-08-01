19063ad01b87780a10abeb9e78cea652b602a7a6
Abetang Joseph <josephabetang@gmail.com>
2019-08-01 12:53:41 +0100

    hash user signup password

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,15 @@
 import { User } from '../models/schema';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const createUser = async (userInfo: any) => {
   const user = new User(userInfo);
   const foundUser = await User.findOne({ email: userInfo.email });
 
   if (!foundUser) {
-    return await user.save();
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    return user.save();
   }
 
   throw Error('This user already exists');
