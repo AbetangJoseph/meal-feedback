30d813660f2bfbe1b950d97069d62cd2a1ee2ea1
Abetang Joseph <josephabetang@gmail.com>
2019-08-04 00:49:49 +0100

    add mealSchema and token method to userSchema

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-import { UserInterface } from '../typings/';
+// import { UserInterface } from '../typings/';
+import { ObjectID } from 'bson';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
   firstName: String,
@@ -11,6 +13,27 @@ const userSchema = new mongoose.Schema({
   confirmPassword: String
 });
 
-const User = mongoose.model<UserInterface>('Users', userSchema);
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    { firstname: this.firstName, _id: this.id },
+    `${process.env.rate_meal_jwt}`
+  );
+  return token;
+};
 
-export { User };
+const mealSchema = new mongoose.Schema({
+  day: String,
+  meal: [
+    {
+      id: ObjectID,
+      time: String,
+      name: String,
+      vendor: String
+    }
+  ]
+});
+
+const User = mongoose.model<any>('Users', userSchema);
+const Meal = mongoose.model<any>('Meal', mealSchema);
+
+export { User, Meal };
