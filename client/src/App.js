e7a337951f740e77fbe0fcb86622fedab34eff7d
Abetang Joseph <josephabetang@gmail.com>
2019-08-04 01:17:31 +0100

    add apolloProvider and wrap app

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 // import Jumbotron from './components/Jumbotron';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 
+const uri = 'http://localhost:3001/graphql';
+
+const client = new ApolloClient({
+  uri
+});
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes />
+      </Router>
+    </ApolloProvider>
   );
 }
 
